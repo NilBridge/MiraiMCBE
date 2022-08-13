09563d8c9f7019761d6339d2098ce4a6f9914015
Sbaoor <2959435045@qq.com>
2022-08-13 09:39:39 +0800

    upload README.md

---
+++ pack.bat
@@ -0,0 +1,16 @@
+chcp 65001
+
+rd tmp /s
+
+: set /p v=[%date% %time%] 请输入版本号：
+
+md tmp
+md "tmp/MiraiMCBE"
+
+: 如果要复制.git文件夹就加上 /h 参数
+xcopy src "tmp/MiraiMCBE" /s /e /exclude:%cd%\pack.config
+: /exclude:%cd%\pack.config
+
+7z a tmp/MiraiMCBE.zip tmp/MiraiMCBE
+
+pause
